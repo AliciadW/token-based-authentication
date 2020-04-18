cc753046436a8915af4a3bdf585a4ec8348d45b6
AliciadW <alicia.dewet@living-stratus.com>
2020-04-18 13:20:08 +0200

    Lesson 3 - 2

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    user: null
+  },
+  mutations: {
+    SET_USER_DATA(state, userData) {
+      state.user = userData
+      localStorage.setItem('user', JSON.stringify(userData))
+    }
+  },
+  actions: {
+    register({ commit }, credentials) {
+      return axios
+        .post('//localhost:3000/register', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
+  }
 })
