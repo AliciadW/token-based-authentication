9e3ce4c2b115a3354e1ad3a966263e803a2098e3
AliciadW <alicia.dewet@living-stratus.com>
2020-04-19 10:32:36 +0200

    Lesson 6 - 2

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/login', (req, res) => {
       name: userInfo.name
     })
   } else {
-    res.sendStatus(400)
+    res.status(401).json({ error: 'Invalid login. Please try again.' }) // send error if credentials don't match record
   }
 })
 
